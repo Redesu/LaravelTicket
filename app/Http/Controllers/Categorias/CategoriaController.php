4bc942502a51fc194a008964908658aa0bedfdd5
Willyan David <62126803+Redesu@users.noreply.github.com>
2025-09-05 13:37:27 -0300

    Added services and Requests to the functions
    Need to test each

--- app/Http/Controllers/Categorias/CategoriaController.php
+++ app/Http/Controllers/Categorias/CategoriaController.php
@@ -2,10 +2,19 @@
 
 namespace App\Http\Controllers\Categorias;
 
+use App\DTOs\CategoriaManagement\Requests\CreateCategoriaRequestDTO;
+use App\DTOs\CategoriaManagement\Requests\DeleteCategoriaRequestDTO;
+use App\DTOs\CategoriaManagement\Requests\UpdateCategoriaRequestDTO;
 use App\DTOs\DataTable\DataTableRequestDTO;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\DataTableCategoriaRequest;
+use App\Http\Requests\DeleteCategoriaRequest;
+use App\Http\Requests\StoreCategoriaRequest;
+use App\Http\Requests\UpdateCategoriaRequest;
+use App\Services\CategoriaManagement\CategoriaCreateService;
 use App\Services\CategoriaManagement\CategoriaDataTableService;
+use App\Services\CategoriaManagement\CategoriaDeleteService;
+use App\Services\CategoriaManagement\CategoriaUpdateService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -14,6 +23,9 @@ class CategoriaController extends Controller
 
     public function __construct(
         private CategoriaDataTableService $dataTableService,
+        private CategoriaCreateService $categoriaCreateService,
+        private CategoriaDeleteService $categoriaDeleteService,
+        private CategoriaUpdateService $categoriaUpdateService
     ) {
     }
 
@@ -28,21 +40,29 @@ class CategoriaController extends Controller
         $response = $this->dataTableService->getCategoriasFromDataTable($dataTableRequest);
 
         return $response->toJsonResponse();
-
     }
 
-    public function insertCategoria()
+    public function insertCategoria(StoreCategoriaRequest $request): JsonResponse
     {
+        $categoriaDTO = CreateCategoriaRequestDTO::fromRequest($request);
+        $result = $this->categoriaCreateService->criarCategoria($categoriaDTO);
 
+        return $result->toJsonResponse();
     }
 
-    public function updateCategoria()
+    public function updateCategoria(UpdateCategoriaRequest $request, $id): JsonResponse
     {
+        $updateDTO = UpdateCategoriaRequestDTO::fromRequest($request);
+        $result = $this->categoriaUpdateService->updateCategoria($updateDTO, $id);
 
+        return $result->toJsonResponse();
     }
 
-    public function deleteCategoria()
+    public function deleteCategoria(DeleteCategoriaRequest $request): JsonResponse
     {
+        $deleteDTO = DeleteCategoriaRequestDTO::fromRequest($request);
+        $result = $this->categoriaDeleteService->deleteCategoria($deleteDTO);
 
+        return $result->toJsonResponse();
     }
 }
