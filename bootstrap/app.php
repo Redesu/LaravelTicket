9519ef824ce85484ccf1ac6c699038a272041197
Willyan David <62126803+Redesu@users.noreply.github.com>
2025-12-04 22:52:42 -0300

    Adds Csrf exception to logout route

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->web(append: [SecurityHeaders::class]);
+        $middleware->web(append: [SecurityHeaders::class])
+            ->validateCsrfTokens(except: [
+                '/auth/logout',
+            ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
