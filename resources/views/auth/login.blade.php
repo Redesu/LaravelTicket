0d63a1131d842fdf8ce664e8ab73d438e32f146d
Willyan David <62126803+Redesu@users.noreply.github.com>
2025-12-04 22:52:28 -0300

    Adds required to login fields

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,7 +13,7 @@
     {{-- Email field --}}
     <div class="input-group mb-3">
         <input type="email" name="email" class="form-control @error('email') is-invalid @enderror"
-            value="{{ old('email') }}" placeholder="{{ __('adminlte::adminlte.email') }}" autofocus>
+            value="{{ old('email') }}" placeholder="{{ __('adminlte::adminlte.email') }}" autofocus required>
 
         <div class="input-group-append">
             <div class="input-group-text">
@@ -31,7 +31,7 @@
     {{-- Password field --}}
     <div class="input-group mb-3">
         <input type="password" name="password" class="form-control @error('password') is-invalid @enderror"
-            placeholder="{{ __('adminlte::adminlte.password') }}">
+            placeholder="{{ __('adminlte::adminlte.password') }}" required>
 
         <div class="input-group-append">
             <div class="input-group-text">
