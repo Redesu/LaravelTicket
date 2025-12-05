deb9322c0cdb0609a2563e11443e6f81373504ec
Willyan David <62126803+Redesu@users.noreply.github.com>
2025-12-05 00:02:31 -0300

    Adde unit test and fixes exampletest

---
+++ tests/Feature/Auth/RegistrationTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use App\Models\User;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class RegistrationTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @test
+     */
+    public function test_user_can_register()
+    {
+
+        $response = $this->post('auth/register', [
+            'name' => 'Test User',
+            'email' => 'test@exampletest.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
+
+        $response->assertRedirect('/');
+        $this->assertDatabaseHas('users', [
+            'email' => 'test@exampletest.com',
+        ]);
+        $this->assertAuthenticated();
+    }
+}
