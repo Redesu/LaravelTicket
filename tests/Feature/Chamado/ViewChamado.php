b1677d2f4ee86ca99f6c2de673a0db5e1521d75e
Willyan David <62126803+Redesu@users.noreply.github.com>
2025-12-08 13:03:45 -0300

    Creates ViewChamado test

---
+++ tests/Feature/Chamado/ViewChamado.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature\Chamado;
+
+use App\Models\Chamado;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ViewChamado extends TestCase
+{
+
+    use RefreshDatabase;
+
+    public function test_user_can_view_chamado(): void
+    {
+
+        $this->seed();
+        $this->withoutExceptionHandling();
+        $user = User::factory()->create([
+            'name' => 'Joao',
+            'email' => 'joao@gmail.com',
+            'password' => bcrypt('password')
+        ]);
+
+        $chamado = Chamado::factory()->create([
+            'titulo' => 'Resetar senha',
+            'descricao' => 'Preciso resetar minha senha',
+            'prioridade' => 'alta',
+            'departamento_id' => '1',
+            'categoria_id' => '2',
+            'user_id' => '1',
+            'created_by' => '1'
+        ]);
+
+        $response = $this->actingAs($user)->get('/admin/chamados/' . $chamado->id);
+
+        $response->assertStatus(200);
+
+    }
+}
