7c7aacda51faa081f669cc361ad73e4cd4a5946d
Willyan David <62126803+Redesu@users.noreply.github.com>
2025-12-08 13:03:36 -0300

    Adds missing useFactory

--- app/Models/Departamento.php
+++ app/Models/Departamento.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Departamento extends Model
 {
-    //
+    use HasFactory;
+
     protected $table = 'departamentos';
     protected $fillable = ['nome', 'descricao'];
 
